0627805cc2cf92c8f9224367170b5dcb71bf7865
Jack Trudan <jackt149@gmail.com>
2023-07-27 11:07:18 -0400

    Added additional test for getPrimes

--- arithmetic.test.ts
+++ arithmetic.test.ts
@@ -7,4 +7,9 @@ describe('arithmetic module', () => {
         expect(getPrimes(10)).toEqual([2,3,5,7])
     })
 
+    test('gets all primes up to 100', () => {
+        expect(getPrimes(100)).toEqual([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,
+            53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+    })
+
 })
